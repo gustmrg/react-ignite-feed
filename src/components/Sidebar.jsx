6d56daae85837b967bd7ba15c827a2359b02c5b8
Gustavo Miranda <gmiranda90@outlook.com>
2023-08-01 00:01:26 -0300

    Add project initial setup

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from "phosphor-react";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://www.github.com/gustmrg.png"
+        />
+        <strong>Gustavo Miranda</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
