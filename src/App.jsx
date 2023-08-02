67819bdcb3870c8c3c40b7ff3f6d2e9c6d7929da
Gustavo Miranda <gmiranda90@outlook.com>
2023-08-01 23:05:28 -0300

    Add Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
 
 import "./global.css";
@@ -12,7 +13,10 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main></main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
---
+++ src/components/Post.jsx
@@ -0,0 +1,41 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://www.github.com/gustmrg.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Gustavo Miranda</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time
+          title="1o de Agosto de 2023 às 23h01"
+          dateTime="2023-08-01 23:03:42"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉<a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
