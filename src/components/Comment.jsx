81bf656d141e0076d1c25895673ae004e2085a9b
Gustavo Miranda <gmiranda90@outlook.com>
2023-08-04 01:05:51 -0300

    Add Avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,16 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 
 import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://www.github.com/gustmrg.png" alt="" />
+      <Avatar
+        hasBorder={false}
+        src="https://www.github.com/gustmrg.png"
+        alt=""
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://www.github.com/gustmrg.png"
-          />
+          <Avatar src="https://www.github.com/gustmrg.png" />
           <div className={styles.authorInfo}>
             <strong>Gustavo Miranda</strong>
             <span>Web Developer</span>
