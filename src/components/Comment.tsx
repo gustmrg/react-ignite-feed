2894e4770b7bb428d530bd6bd00e32ec1ccb88b3
Gustavo Miranda <gmiranda90@outlook.com>
2023-08-05 20:08:16 -0300

    Add TypeScript

---
+++ src/components/Comment.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeCount() {
+    setLikeCount(likeCount + 1);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://www.github.com/gustmrg.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo Miranda</strong>
+              <time
+                title="02 de Agosto de 2023 às 21h23"
+                dateTime="2023-08-02 21:23:47"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Apagar comentário" onClick={handleDeleteComment}>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeCount}>
+            <ThumbsUp />
+            Curtir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://www.github.com/gustmrg.png" />
+        <strong>Gustavo Miranda</strong>
+        <span>Software Engineer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
